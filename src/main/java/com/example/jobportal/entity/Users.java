f1d86917419c399dafe97d1e7900383b1828ec76
Joshua Chuah <xjoshua96@gmail.com>
2025-01-04 16:46:47 -0500

    entity

---
+++ src/main/java/com/example/jobportal/entity/Users.java
@@ -0,0 +1,92 @@
+package com.example.jobportal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
+    private UsersType userTypeId;
+
+    public Users() {
+
+    }
+
+    public Users(int userId, Date registrationDate, boolean isActive, String password, String email, UsersType userTypeId) {
+        this.userId = userId;
+        this.registrationDate = registrationDate;
+        this.isActive = isActive;
+        this.password = password;
+        this.email = email;
+        this.userTypeId = userTypeId;
+    }
+
+    public UsersType getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(UsersType userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
