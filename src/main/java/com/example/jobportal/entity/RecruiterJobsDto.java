81f91376b837165c7195de34f9e64f3751df2198
Joshua Chuah <xjoshua96@gmail.com>
2025-01-14 21:01:18 -0500

    recruiter jobs

---
+++ src/main/java/com/example/jobportal/entity/RecruiterJobsDto.java
@@ -0,0 +1,58 @@
+package com.example.jobportal.entity;
+
+public class RecruiterJobsDto {
+
+    private Long totalCandidates;
+    private Integer jobPostId;
+    private String jobTitle;
+    private JobLocation jobLocationId;
+    private JobCompany jobCompanyId;
+
+    public RecruiterJobsDto(Long totalCandidates, JobCompany jobCompanyId, JobLocation jobLocationId, Integer jobPostId, String jobTitle) {
+        this.totalCandidates = totalCandidates;
+        this.jobCompanyId = jobCompanyId;
+        this.jobLocationId = jobLocationId;
+        this.jobPostId = jobPostId;
+        this.jobTitle = jobTitle;
+    }
+
+    public Long getTotalCandidates() {
+        return totalCandidates;
+    }
+
+    public void setTotalCandidates(Long totalCandidates) {
+        this.totalCandidates = totalCandidates;
+    }
+
+    public JobCompany getJobCompanyId() {
+        return jobCompanyId;
+    }
+
+    public void setJobCompanyId(JobCompany jobCompanyId) {
+        this.jobCompanyId = jobCompanyId;
+    }
+
+    public JobLocation getJobLocationId() {
+        return jobLocationId;
+    }
+
+    public void setJobLocationId(JobLocation jobLocationId) {
+        this.jobLocationId = jobLocationId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public Integer getJobPostId() {
+        return jobPostId;
+    }
+
+    public void setJobPostId(Integer jobPostId) {
+        this.jobPostId = jobPostId;
+    }
+}
