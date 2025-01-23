3442562e8f64289515a76e206d1d0ab9ba16489e
Joshua Chuah <xjoshua96@gmail.com>
2025-01-23 15:09:34 -0500

    service for applying to jobs

---
+++ src/main/java/com/example/jobportal/services/JobSeekerApplyService.java
@@ -0,0 +1,28 @@
+package com.example.jobportal.services;
+
+import com.example.jobportal.entity.JobSeekerApply;
+import com.example.jobportal.entity.JobSeekerProfile;
+import com.example.jobportal.repository.JobSeekerApplyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobSeekerApplyService {
+
+    private final JobSeekerApplyRepository jobSeekerApplyRepository;
+
+    @Autowired
+    public JobSeekerApplyService(JobSeekerApplyRepository jobSeekerApplyRepository) {
+        this.jobSeekerApplyRepository = jobSeekerApplyRepository;
+    }
+
+    public List<JobSeekerApply> getCandidatesJobs(JobSeekerProfile userAccountId){
+        return jobSeekerApplyRepository.findByUserId(userAccountId);
+    }
+
+    public List<JobSeekerApply> getJobCandidates(JobPostActivity job){
+        return jobSeekerApplyRepository.findByJob(job);
+    }
+}
