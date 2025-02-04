8c215574de24513bba30c2d597ebaac23a1b3928
Joshua Chuah <xjoshua96@gmail.com>
2025-02-03 22:33:10 -0500

    updated controller

--- src/main/java/com/example/jobportal/controller/JobSeekerApplyController.java
+++ src/main/java/com/example/jobportal/controller/JobSeekerApplyController.java
@@ -1,30 +1,55 @@
 package com.example.jobportal.controller;
 
-import com.example.jobportal.entity.JobPostActivity;
-import com.example.jobportal.services.JobPostActivityService;
-import com.example.jobportal.services.UsersService;
+import com.example.jobportal.entity.*;
+import com.example.jobportal.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class JobSeekerApplyController {
 
     private final JobPostActivityService jobPostActivityService;
     private final UsersService usersService;
+    private final JobSeekerApplyService jobSeekerApplyService;
+    private final JobSeekerSaveService jobSeekerSaveService;
+    private final RecruiterProfileService recruiterProfileService;
+    private final JobSeekerProfileService jobSeekerProfileService;
 
     @Autowired
-    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService) {
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService,
+                                    JobSeekerSaveService jobSeekerSaveService, JobSeekerApplyService jobSeekerApplyService,
+                                    RecruiterProfileService recruiterProfileService, JobSeekerProfileService jobSeekerProfileService) {
         this.jobPostActivityService = jobPostActivityService;
         this.usersService = usersService;
+        this.jobSeekerSaveService = jobSeekerSaveService;
+        this.jobSeekerApplyService = jobSeekerApplyService;
+        this.recruiterProfileService = recruiterProfileService;
+        this.jobSeekerProfileService = jobSeekerProfileService;
     }
 
     @GetMapping("job-details-apply/{id}")
     public String display(@PathVariable("id") int id, Model model) {
         JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+        List<JobSeekerApply> jobSeekerApplyList = jobSeekerApplyService.getJobCandidates(jobDetails);
+        List<JobSeekerSave> jobseekerSaveList = jobSeekerSaveService.getJobCandidates(jobDetails);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                RecruiterProfile user = recruiterProfileService.getCurrentRecruiterPrfile();
+            }
+        }
         model.addAttribute("jobDetails", jobDetails);
         model.addAttribute("user", usersService.getCurrentUserProfile());
         return "job-details";
